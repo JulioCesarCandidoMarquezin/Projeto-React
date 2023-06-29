12eeb080f3bb783fbbd0a21af3be4f2f6dd8d7d6
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-06-29 17:11:21 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,15 @@
 import { useState } from 'react';
-import ReactFlow, {
-  MiniMap,
-  Background,
-  Controls,
-  useNodesState,
-  Node,
-} from 'reactflow';
+import ReactFlow, { Background, Controls } from 'reactflow';
 import 'reactflow/dist/style.css';
-import { Entity } from './components/Entity';
+import Entity from './components/Entity';
 
 function App() {
-  const [nodes, setNodes] = useNodesState([]);
-  const nodeTypes = {
-    entity: Entity,
-  };
-
-  const initialNodes = [
-    {
-      id: crypto.randomUUID(),
-      type: 'entity',
-      position: {
-        x: '100px',
-        y: '100px',
-      },
-      data: {},
-    },
-  ];
-
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} nodeTypes={nodeTypes}>
+      <ReactFlow>
         <Entity name={'Entity'} />
         <Background />
         <Controls />
-        <MiniMap />
       </ReactFlow>
     </div>
   );
