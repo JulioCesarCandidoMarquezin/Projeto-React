fe74e57343e66f70dc05a913180caf62c5851766
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-06-30 14:18:15 -0400

    Versão que funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import ReactFlow, { MiniMap, Background, Controls } from 'reactflow';
+import { useState, useCallback } from 'react';
+import ReactFlow, { ConnectionMode, addEdge, applyNodeChanges, applyEdgeChanges, MiniMap, Background, Controls, useEdgesState } from 'reactflow';
 import 'reactflow/dist/style.css';
 import Entity from './components/Entity';
+import { Toolbar } from '@radix-ui/react-toolbar';
 
 function App() {
   const nodeTypes = {
@@ -9,21 +10,44 @@ function App() {
   };
   const initialNodes = [
     {
-      id: crypto.randomUUID(),
+      id: 'entity1',
       type: 'entity',
       position: {
         x: 100,
         y: 100,
       },
-      data: { name: 'Entity' },
+      data: { name: 'Entity 1' },
     },
+    {
+      id: 'entity2',
+      type: 'entity',
+      position: {
+        x: 600,
+        y: 100,
+      },
+      data: { name: 'Entity 2' },
+    }
   ];
 
-  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [nodes, setNodes, onNodesChange] = useEdgesState(initialNodes)
+
+  const onConnect = useCallback((connection) =>{
+    return setEdges(edges => addEdge(connection, edges))
+  })
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodeTypes={nodeTypes} nodes={nodes}>
+      <ReactFlow 
+      nodeTypes={nodeTypes} 
+      nodes={nodes} 
+      onNodesChange={onNodesChange}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      connectionMode={ConnectionMode.Loose}
+      fitView={true}
+      >
         <Background />
         <Controls />
         <MiniMap />
--- src/components/Entity.jsx
+++ src/components/Entity.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Entity.css';
+import { Handle, Position } from 'reactflow';
 
 export default function Entity(props) {
   const [attributes, setAttributes] = useState([]);
@@ -10,7 +11,7 @@ export default function Entity(props) {
   useEffect(() => {}, [foreingKeys]);
 
   function addNewAttribute() {
-    const newAttribute = 'new attribute';
+    const newAttribute = "sla";
     setAttributes([...attributes, newAttribute]);
   }
 
@@ -20,32 +21,58 @@ export default function Entity(props) {
   }
 
   return (
-    <div id={'Entity'}>
-      <label>{props.name}</label>
+    <div id={'entity'}>
+      <Handle 
+      id='Top' 
+       
+      position={Position.Top}
+      />
+      <Handle 
+      id='Bottom' 
+       
+      position={Position.Bottom}
+      />
+      <Handle 
+      id='Right' 
+       
+      position={Position.Right}
+      />
+      <Handle 
+      id='Left' 
+       
+      position={Position.Left}
+      />
 
-      <hr />
+      <div id='container'>
+      <label>{props.data.name}</label>
 
-      <ul>
-        {attributes.map((attribute, index) => {
-          return <li key={index}>{attribute}</li>;
-        })}
-      </ul>
+<hr />
 
-      <button id={'addNewAttribute'} onClick={addNewAttribute}>
-        + Adicionar novo atributo
-      </button>
+<ul>
+  {attributes.map((attribute, index) => {
+    return <li key={index}>{attribute}:<label></label></li>;
+  })}
+</ul>
 
-      <hr />
+<div id='newAttribute'>
+  <button id={'addNewAttribute'} onClick={addNewAttribute}>+</button>
+  <input id='name' type='text' placeholder='name'></input>
+  <input id='type' type='text' placeholder='type'></input>
+</div>
 
-      <ul>
-        {foreingKeys.map((foreingKey, index) => {
-          return <li key={index}>{foreingKey}</li>;
-        })}
-      </ul>
+<hr />
 
-      <button id={'addNewForeingKey'} onClick={addNewForeingKey}>
-        + Adicionar nova chave estrangeira
-      </button>
+<ul>
+  {foreingKeys.map((foreingKey, index) => {
+    return <li key={index}>{foreingKey}</li>;
+  })}
+</ul>
+
+<button id={'addNewForeingKey'} onClick={addNewForeingKey}>
+  + Adicionar nova chave estrangeira
+</button>
+      </div>
+      
     </div>
   );
 }
